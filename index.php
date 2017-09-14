62b76a15f54349fd79fa464468ce7a3ee5cf4822
jaesgit <985451252@qq.com>
2017-09-14 15:02:57 +0800

    trait

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+class base{
+	public function sayHello(){
+		echo "Hello";
+	}
+}
+trait sayPhp{
+	public function sayHello(){
+		parent::sayHello();
+		echo ' php';
+	}
+}
+trait hello{
+	public function hello(){
+		echo 'Hello';
+	}
+}
+trait world{
+	public function world(){
+		echo ' world!';
+	}
+}
+class myHelloPhp extends base{
+	use sayPhp,hello,world;
+	public function sayHello(){
+		parent::sayHello();
+		echo ' world';
+	}
+	public function world(){
+		echo ' php!';
+	}
+}
+$o = new myHelloPhp();
+//$o->sayHello();
+$o->hello();
+$o->world();
